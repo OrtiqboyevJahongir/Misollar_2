4614094165550c3b8c0f7cdf08ed632ea29ffb31
Jahongir Ortiqboyev <108238210+OrtiqboyevJahongir@users.noreply.github.com>
2022-10-28 18:51:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,189 @@
+"use strict"
+
+        //             M-1
+
+        // let n = +prompt("sonni kiriting"),
+        //     k = +prompt("sonni kiriting")
+            
+
+            // for(let i=1 ; i<=n ; i++){
+            //     console.log(k);
+            // }
+
+                        // M-2
+         
+        // let a= +prompt("sonni kiriting"),
+        //     b = +prompt("sonni kiriting")   
+
+        //     for(let i=b ; i<=a ; i++){
+
+        //       console.log(b);
+        //         b++
+        //     }
+                      //  M-3
+
+            //  let a= +prompt("sonni kiriting"),
+            //      b = +prompt("sonni kiriting")   
+            //      for(let i=b ; i<=a ; i++){
+            //              console.log(b-i);
+            //               b++
+            //           }          
+                        //M-4
+
+
+            // let a= +prompt("sonni kiriting")
+              
+
+            //   for(let i=1 ; i<=a ; i++){
+                       
+            //             console.log(i*10000);
+            //     }                      
+
+                        // M-5
+                
+            // let a= +prompt("sonni kiriting"),
+            //     s=1
+            //     for(let i=1 ; i<=a; i++){
+                        
+            //             console.log(i/10*s*10000);
+            //     }                    
+                        // M-6  
+            //  let a=2,
+            //     s=1 
+            //     for(let i=1 ; i<=a; i++){
+            //         if(i%2==0){
+            //             s=i/10+1
+            //             console.log(s*10000);
+            //         }
+                       
+            //     }   
+                         
+                            // M-7
+
+              
+            // let a= +prompt("sonni kiriting"),
+            //      b = +prompt("sonni kiriting"),  
+            //      c=0
+            //  for(let i=b ; i<=a ; i++){
+            //          c+=i      
+            //       }console.log(c);     
+
+                            // M-8
+
+            // let a= +prompt("sonni kiriting"),
+            //      b = +prompt("sonni kiriting"),  
+            //      c=1
+            //  for(let i=b ; i<=a ; i++){
+            //          c*=i      
+            //       }console.log(c);
+
+                            //M-9
+                            
+            // let  a= +prompt("sonni kiriting"),
+            //      b = +prompt("sonni kiriting"),  
+            //       d=0
+            //  for(let i=b ; i<=a ; i++){
+            //             d+=i**2
+                       
+            //       }console.log(d);
+
+                            // M-10
+
+            //    let  a= +prompt("sonni kiriting"),
+                    
+            //         d=0
+            //     for(let i=1 ; i<=a ; i++){
+            //             d+=1/i
+                       
+            //       }console.log(d);       
+            
+                            // M-11
+
+                  
+            // let a= +prompt("sonni kiriting")
+            //  for(let i=1 ; i<=a ; i+=2){
+            //             console.log(i);
+            //       } 
+
+                           // M-12
+
+                                   
+            // let a= +prompt("sonni kiriting")
+            //  for(let i=0 ; i<=a ; i+=2){
+            //             console.log(i);
+            //       } 
+
+                          //  M-13
+
+            // let a= +prompt("sonni kiriting")
+            //  for(let i=4 ; i<=a ; i++){
+            //             if(i%2==0 && i%i==0 && i%1==0 || i%3==0 && i%i==0 && i%1==0)
+            //             console.log(i);
+            //       } 
+
+                          // M-14
+                          
+                          
+                //  let a= +prompt("sonni kiriting")
+                     
+                //     for(let i=1 ; i<=a ; i++){
+                //          if(i%2==0 ){
+                //             console.log("juft sonlar "+i**2);
+                //          }else  console.log( "toq sonlar "+i**2);}
+                //          {
+                            
+                //       } 
+                         
+                           // M-15
+
+                //    let a= +prompt("sonni kiriting")
+                //        for (let i = 0; i < a; i++) {
+                //          console.log(a-i);
+ 
+                //        }       
+                            // M-16
+                        //    let a= +prompt("sonni kiriting")
+                        //         for(let i=1 ; i<=a ; i++){
+                        //          if(i%5!=0 ){
+                        //             console.log(i);
+                        //          }
+                        //  }
+                        
+                            // M-17
+
+                        // let a= +prompt("sonni kiriting"),
+                        //      b=1
+                        //      for (let i = 1; i <=a; i++) {
+                        //             b*=i
+
+                        //      }console.log(b);
+                             
+                             // M-18
+
+                        // let a= +prompt("sonni kiriting"),
+                        //      b=1, s=0
+                        //      for (let i = 1; i <=a; i++) {
+                        //             b*=i
+                        //             s+=b
+
+                        //      }console.log(s);
+                                
+                            //  M-19
+
+                        // let a= +prompt("sonni kiriting"),
+                        //      x= +prompt("sonni kiriting")
+                        //      b=0, s=1
+                        //      for (let i = 1; i <=a; i++) {
+                        //             b+=1+x+(x**i )/(s*i)
+                        //             console.log(b);
+                        //      }
+
+                            //  M-19
+                        
+                        // let n= +prompt("sonni kiriting"),
+                        //      x= +prompt("sonni kiriting")
+                        //    for (let i = 1; i <=n; i++){
+                        //         if(i%x==0){
+                        //             console.log(i);
+                        //         }
+                        //    }   
